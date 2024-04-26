778c9eed5b669975a30388774a27ffcc5700b31a
Narisen Lyu <nlyu3@wisc.edu>
2024-04-26 13:42:22 -0500

    implemented delete

--- delete.C
+++ delete.C
@@ -1,9 +1,18 @@
 #include "catalog.h"
 #include "query.h"
+#include <stdexcept>
+#include <cassert>
 
 
-/*
- * Deletes records from a specified relation.
+/**
+ * Deletes records from a specified relation. 
+ * This function will delete all tuples in relation
+ * satisfying the predicate specified by attrName, op, 
+ * and the constant attrValue. type denotes the type 
+ * of the attribute. 
+ * 
+ * You can locate all the qualifying tuples using 
+ * a filtered HeapFileScan.
  *
  * Returns:
  * 	OK on success
@@ -16,11 +25,77 @@ const Status QU_Delete(const string & relation,
 		       const Datatype type, 
 		       const char *attrValue)
 {
-// part 6
-return OK;
-
-
-
+//check if input argument is null
+if (relation.empty()){
+	return UNIXERR;
 }
 
+Status status;
+HeapFileScan scanner(relation, status);
+if (status != OK){
+	return status;
+}
 
+//get the offset and length
+AttrCatalog cat(status);
+if (status != OK){
+	return status;
+}
+AttrDesc desc; 
+if (! attrName.empty()){
+	status = cat.getInfo(relation, attrName, desc);
+	if (status != OK){
+		return status;
+	}
+	//assert(desc.attrType == type);
+}
+
+//check if attrName is NULL
+if (attrName.empty()){
+	//scan without filtering
+	//Set startScan’s offset and length to 0, type to string, filter to NULL
+	status = scanner.startScan(0,0, STRING, NULL, op);
+}
+// convert attribute
+// locate all the qualifying tuples using a filtered HeapFileScan.
+else if (type == INTEGER){
+	int attrValueInt = atoi(attrValue);
+	void *ptr = &(attrValueInt);
+	status = scanner.startScan(desc.attrOffset,desc.attrLen, INTEGER, reinterpret_cast<char*> (ptr), op);
+}
+else if (type == FLOAT){
+	int attrValueFloat = atof(attrValue);
+	void *ptr = &(attrValueFloat);
+	status = scanner.startScan(desc.attrOffset,desc.attrLen, FLOAT, reinterpret_cast<char*> (ptr), op);
+}
+else if (type == STRING){
+	status = scanner.startScan(desc.attrOffset,desc.attrLen, STRING, attrValue, op);
+}
+else{
+	throw std::invalid_argument( "Error!!!!!!!!" );
+}
+if (status != OK){
+	return status;
+}
+
+// Iterate over records and delete
+RID rid;
+while (true) {
+	status = scanner.scanNext(rid);
+	if (status == FILEEOF){
+		break;
+	}
+	if (status != OK){
+		return status;
+	}
+	status = scanner.deleteRecord();
+	if (status != OK){
+		return status;
+	}
+}
+status = scanner.endScan();
+if (status != OK){
+	return status;
+}
+return OK;
+}
